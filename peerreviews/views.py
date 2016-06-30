0ac033bf03183ecb8991bfcb0b383d1d538ba789
Shaquille Johnson <sjohns25@nd.edu>
2016-06-30 09:53:17 -0400

    get and post behavior

--- peerreviews/views.py
+++ peerreviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from models import Reviewer
+from models import Reviewer, Author, Submission
 from serializers import ReviewerSerializer
 
 # Create your views here.
@@ -14,3 +14,68 @@ class ReviewerViewSet(viewsets.ModelViewSet):
     """
     queryset = Reviewer.objects.all()
     serializer_class = ReviewerSerializer
+
+@api_view(['GET', 'POST'])
+def reviewer_list(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        reviewers = Reviewer.objects.all()
+        serializer = ReviewerSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ReviewerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows reviewers to be CRUDed.
+    """
+    queryset = Reviewer.objects.all()
+    serializer_class = ReviewerSerializer
+
+@api_view(['GET', 'POST'])
+def author_list(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        reviewers = Author.objects.all()
+        serializer = AuthorSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = AuthorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SubmissionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows reviewers to be CRUDed.
+    """
+    queryset = Submission.objects.all()
+    serializer_class = ReviewerSerializer
+
+@api_view(['GET', 'POST'])
+def submission_list(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        reviewers = Submission.objects.all()
+        serializer = SubmissionSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SubmissionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
